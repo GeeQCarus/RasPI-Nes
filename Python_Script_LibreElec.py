9268d7ba6649dd1b8f5623bda5ef9d8d9a7b9deb
GeeQCarus <flemieux@live.com>
2016-11-17 10:02:54 -0500

    Update Python_Script_LibreElec.py

--- Python_Script_LibreElec.py
+++ Python_Script_LibreElec.py
@@ -1,1 +1,15 @@
-Lorem Ipsum 
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Suspendisse congue lorem ut mi hendrerit rutrum. 
+Curabitur eget dictum ex, sit amet feugiat risus. 
+Maecenas sit amet mauris ut lectus interdum tempus sed quis ligula. 
+Donec vehicula tempus dui, condimentum hendrerit ligula ultrices eu. 
+Fusce laoreet eget mi ut porta. 
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. 
+Nulla vitae eros in mi commodo consectetur sit amet ac urna. 
+Etiam placerat molestie quam, non aliquet metus commodo at. 
+Praesent convallis dui nec elementum sodales. 
+Morbi scelerisque condimentum nisl non condimentum. 
+Proin at risus elit. 
+Aliquam erat volutpat. 
+Suspendisse maximus, libero quis bibendum aliquam, purus nunc congue ipsum, nec interdum lorem dolor a tellus. 
+In eget rutrum lectus, at laoreet orci.
