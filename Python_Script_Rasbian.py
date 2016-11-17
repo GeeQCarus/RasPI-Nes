15fb875074c9cb2e5a930aca011663a368986833
GeeQCarus <flemieux@live.com>
2016-11-17 09:56:39 -0500

    Update Python_Script_Rasbian.py

--- Python_Script_Rasbian.py
+++ Python_Script_Rasbian.py
@@ -1,1 +1,29 @@
-Lorem Ipsum
+
+  
+import RPi.GPIO as GPIO  # import RPi.GPIO module    
+  
+
+GPIO.setmode(GPIO.BCM) # set to BCM numbering
+  
+# Set up Inputs  
+GPIO.setup(16, GPIO.IN)     # set Read kill app 
+GPIO.setup(20, GPIO.IN)     # set Read Reboot PI 
+GPIO.setup(21, GPIO.IN)     # set Read Shutdown PI 
+
+
+while(1)  # create an infinite loop because 1 = TRUE, and while it is true it will run, we don't offer an exit so it will loop all day waiting for inputs!
+  
+if GPIO.input(16):
+    print("pkill retroarch\n")
+
+else:	
+if GPIO.input(20):
+    print("sudo reboot\n")
+
+else:	
+if GPIO.input(21):
+    print("sudo halt\n")
+
+    
+  
+
