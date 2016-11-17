041405d34a7736621bdf4cbef7c8c217369222b4
GeeQCarus <flemieux@live.com>
2016-11-17 09:58:24 -0500

    Update Python_Script_Rasbian.py

--- Python_Script_Rasbian.py
+++ Python_Script_Rasbian.py
@@ -14,15 +14,15 @@ GPIO.setup(21, GPIO.IN)     # set Read Shutdown PI
 while(1)  # create an infinite loop because 1 = TRUE, and while it is true it will run, we don't offer an exit so it will loop all day waiting for inputs!
   
 if GPIO.input(16):
-    print("")
+    print("sudo reboot\n") # no predefined app to kill, setup to reboot anyhow you can delete this line if you wont use it!
 
 else:	
 if GPIO.input(20):
-    print("sudo reboot\n")
+    print("sudo reboot\n")  # safely reboot the system
 
 else:	
 if GPIO.input(21):
-    print("sudo halt\n")
+    print("sudo halt\n") # safely shutdown the system
 
     
   
