eeecb531df4e31223c3682128b5fff3e57639bd0
GeeQCarus <flemieux@live.com>
2016-11-17 10:59:18 -0500

    Update Python_Script_Retropie.py

--- Python_Script_Retropie.py
+++ Python_Script_Retropie.py
@@ -14,12 +14,10 @@ while(1)  # create an infinite loop because 1 = TRUE, and while it is true it wi
 if GPIO.input(16):
     print("pkill retroarch\n") # kill any retroarch based emulator, by default most are, they start with lr you will need to download the right emulator for the N64
 
-else:	
-if GPIO.input(20):
+else if GPIO.input(20):
     print("sudo reboot\n") # safely reboot the pi after holding reset 3 seconds with my arduino code
 
-else:	
-if GPIO.input(21):
+else if GPIO.input(21):
     print("sudo halt\n") # safely shutdown the pi after releasing the power button with my arduino code
 
     
